910ba6c456fc7c8fd8deada51bd0a1df038a5aac
kuzak <kuzakovevgen@gmail.com>
2025-06-28 15:59:42 +0700

    Change return type for UserDao

--- core/database/src/main/java/ru/evgenykuzakov/database/dao/UsersDao.kt
+++ core/database/src/main/java/ru/evgenykuzakov/database/dao/UsersDao.kt
@@ -2,13 +2,12 @@ package ru.evgenykuzakov.database.dao
 
 import androidx.room.Dao
 import androidx.room.Query
-import kotlinx.coroutines.flow.Flow
 import ru.evgenykuzakov.database.model.UserEntity
 
 @Dao
 interface UsersDao {
 
     @Query("SELECT * from users")
-    fun getAllUsers(): Flow<List<UserEntity>>
+    fun getAllUsers(): List<UserEntity>
 
 }
