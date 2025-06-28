8f67f7dc6e256eb1adb21ddd87d597f8525057e6
kuzak <kuzakovevgen@gmail.com>
2025-06-28 14:27:49 +0700

    Creating network layer

---
+++ core/network/src/main/java/ru/evgenykuzakov/network/di/NetworkModule.kt
@@ -0,0 +1,28 @@
+package ru.evgenykuzakov.network.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.evgenykuzakov.network.RandomUserApi
+import javax.inject.Singleton
+
+private const val BASE_URL = "https://randomuser.me/api"
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRandomUserApi(): RandomUserApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(RandomUserApi::class.java)
+    }
+
+}
