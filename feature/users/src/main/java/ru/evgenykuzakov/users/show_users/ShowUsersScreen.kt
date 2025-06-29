69ba16ba7b8e67d15d0bb6f710168cba750ddf24
kuzak <kuzakovevgen@gmail.com>
2025-06-29 16:12:20 +0700

    Start creating UserDetailInfoScreen

--- feature/users/src/main/java/ru/evgenykuzakov/users/show_users/ShowUsersScreen.kt
+++ feature/users/src/main/java/ru/evgenykuzakov/users/show_users/ShowUsersScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -17,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -49,19 +52,24 @@ fun ShowUsersScreen(
             ) {
                 items(items = users.data) {
                     Card(
-                        modifier = Modifier.padding(12.dp),
+                        modifier = Modifier.padding(horizontal = 16.dp),
                         onClick = {}
                     ) {
-                        Row {
+                        Row(
+                            modifier = Modifier.padding(vertical = 4.dp, horizontal = 16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
                             AsyncImage(
                                 model = it.picture.large,
                                 contentDescription = null,
                                 contentScale = ContentScale.Crop,
                                 modifier = Modifier
                                     .clip(CircleShape)
-                                    .size(100.dp)
+                                    .size(120.dp)
                             )
+                            Spacer(modifier = Modifier.width(12.dp))
                             Column(
+                                modifier = Modifier.padding(12.dp),
                                 verticalArrangement = Arrangement.spacedBy(8.dp)
                             ) {
                                 LabelSmallText(
